76a0d2cea7708531cf1c8bf8ac226d76c2edee50
NancyDLL <nano14@mac.com>
2017-08-08 13:53:35 -0500

    Got the first part of the Train Scheduler to work.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,1 +1,44 @@
 console.log("The version 1 js file for Trains is connected.")
+
+// Initialize Firebase
+var config = {
+	apiKey: "AIzaSyA07rB0i1_CaykBeZCxAyDy1VlzMQ-0X4U",
+	authDomain: "test-project-e2b2b.firebaseapp.com",
+	databaseURL: "https://test-project-e2b2b.firebaseio.com",
+	projectId: "test-project-e2b2b",
+	storageBucket: "test-project-e2b2b.appspot.com",
+	messagingSenderId: "86122413164"
+};
+
+firebase.initializeApp(config);
+
+var database = firebase.database();
+
+//Button for adding Trains
+$("#add-train-btn").on("click", function(event) {
+  	event.preventDefault();
+
+  	// Grabs user input
+  	var trainName = $("#train-input").val().trim();
+  	var destinationName = $("#destination-input").val().trim();
+  	var timeStart = moment($("#time-input").val().trim(), "HH:mm").format("X");
+  	var frequencyRate = $("#frequency-input").val().trim();
+
+  	// Creates local "temporary" object for holding train data
+  	var newTrain = {
+    	name: trainName,
+    	destination: destinationName,
+    	start: timeStart,
+    	frequency: frequencyRate
+  	};
+
+// Uploads train data to the database
+database.ref().push(newTrain);
+
+// Logs train data to console
+console.log(newTrain.name);
+console.log(newTrain.destination);
+console.log(newTrain.start);
+console.log(newTrain.frequency);
+
+});
